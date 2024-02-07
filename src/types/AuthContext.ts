f5eebd6a0f6cf367e70ee997ec9202b2e7bf21db
Douglas Souza <lyricsan@hotmail.com>
2024-02-07 11:31:18 -0300

    add auth context type

---
+++ src/types/AuthContext.ts
@@ -0,0 +1,8 @@
+import { User, UserCredential } from 'firebase/auth';
+
+export type AuthContextType = {
+  currentUser: User | null;
+  signUp: (email: string, password: string) => Promise<UserCredential>;
+  login: (email: string, password: string) => Promise<UserCredential>;
+  logout: () => Promise<void>;
+};
