0b9898486f14b86801fad668380cc8050a02734c
Douglas Souza <lyricsan@hotmail.com>
2024-02-07 12:32:20 -0300

    remove conditional and add protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { useAuth } from './context/AuthContext';
 import HomePage from './pages/HomePage';
 import TodoPage from './pages/TodoPage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
-  const currentUser = useAuth();
-
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={currentUser?.currentUser ? <TodoPage /> : <HomePage />}
-        />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route
+          path="/todo"
+          element={
+            <ProtectedRoute>
+              <TodoPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
