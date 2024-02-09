7083d6afcf4ad599f8f4ca3ff922373fed80b87a
Douglas Souza <lyricsan@hotmail.com>
2024-02-09 14:36:19 -0300

    chore: add tasks

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+import Modal from "./Modal";
+
 export default function Todo() {
-  return <div>Todo</div>;
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  return (
+    <div className="container mx-auto flex flex-col mt-6 px-6">
+      <button
+        onClick={() => setOpenModal(true)}
+        className="self-end rounded p-2 text-white font-medium bg-pink-500"
+      >
+        New task
+      </button>
+
+      {openModal && <Modal handleCloseModal={() => setOpenModal(false)} />}
+    </div>
+  );
 }
