6e160640c2e31dae887845dab4e6b45d24c82f89
Douglas Souza <lyricsan@hotmail.com>
2024-02-09 14:14:59 -0300

    add logout function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
+import { useAuth } from "../hooks/useAuth";
+
 export default function Header() {
+  const auth = useAuth();
+
   return (
     <header className="shadow">
       <div className="container mx-auto flex items-center justify-between py-7 px-6">
         <h1 className="text-2xl font-semibold">ToDo</h1>
 
-        <button>Log out</button>
+        <button onClick={() => auth?.logout()}>Log out</button>
       </div>
     </header>
   );
