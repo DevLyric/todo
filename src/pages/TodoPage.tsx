94f82e0db4db52d1d32c629c3ed22fbc84744d4e
Douglas Souza <lyricsan@hotmail.com>
2024-02-10 13:46:23 -0300

    add navbar in todo page

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,10 +1,10 @@
-import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 import Todo from "../components/Todo";
 
 export default function TodoPage() {
   return (
     <div>
-      <Header />
+      <Navbar />
       <Todo />
     </div>
   );
